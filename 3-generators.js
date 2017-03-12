7fca8e3bbffd2d419987f71354bff142a082a89a
Vasa <vasanthk@users.noreply.github.com>
2017-03-11 18:39:58 -0800

    Using JavaScript (ES6) generators with Promises to handle async flows

--- 3-generators.js
+++ 3-generators.js
@@ -185,7 +185,31 @@ function *main() {
 var it = main();
 it.next(); // get it all started
 
+/**
+* Using JavaScript (ES6) generators with Promises to handle async flows
+* https://egghead.io/lessons/javascript-use-javascript-es6-generators-with-promises-to-handle-async-flows
+*/
 
+function* createQuoteFetcher() {
+  const response = yield fetch('http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json')
+  const quote = yield response.json()
+  return `${quote.quoteText} —${quote.quoteAuthor}`
+}
+
+const coroutine = (gen) => {
+  const generator = gen()
+
+  const handle = (result) => {
+    if (result.done) return Promise.resolve(result.value)
+    return Promise.resolve(result.value)
+      .then(res => handle(generator.next(res)))
+	}
+
+  return handle(generator.next())
+}
+
+const quoteFetcher = coroutine(createQuoteFetcher)
+quoteFetcher.then(quote => console.log(quote))
 
 
 // 1. Generators as iterators (data production)
